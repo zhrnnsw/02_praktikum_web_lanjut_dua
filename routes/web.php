0753995dffcb9d76671770ffa9ea5fc2e574932b
Zahra Annisa Wahono <zahraannisawahono08@gmail.com>
2023-02-22 09:13:37 +0700

    delete PageController and change web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +16,8 @@ use App\Http\Controllers\PageController;
 |
 */
 
-Route::get('/', [PageController::class, 'index']); 
+Route::get('/', [HomeController::class, 'index']); 
 
-Route::get('/about', [PageController::class, 'about']); 
+Route::get('/about', [AboutController::class, 'about']); 
 
-Route::get('/articles/{id}', [PageController::class, 'articles']); 
+Route::get('/articles/{id}', [ArticleController::class, 'articles']); 
